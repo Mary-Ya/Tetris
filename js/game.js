732cfa57e90380b78dd5ffb02d29868abf147fdf
Mary <Mary-Ya@users.noreply.github.com>
2015-06-23 11:45:59 +0800

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,30 +1,45 @@
 var Game = function() {
-    // new game started
+
+    // game created on window.onload
+
     this.scores = 0;
-    this.speed = 1000;
+    this.speed = settings.normal;
+    this.speedDifference = 100 - settings.normal/10;
+    /*
+    "normal": 800,
+    "fast":500,
+    "crazy":200
+    */
     this.over = true;
     this.pause = false;
-    this.tetroBar = [0, 1, 2, 3, 4, 5, 6];
-    this.nextTetroBar = [0, 1, 2, 3, 4, 5, 6];
-    this.mainScene = new Scene(20, 10);
-    this.pausedScene = new Scene(20, 10);
+    this.tetroBar = settings.defoultTetroBar;
+    this.nextTetroBar = settings.defoultTetroBar;
+    this.mainScene = new Scene(settings.sceneHeight, settings.sceneWidth);
+    this.pausedScene = new Scene(settings.sceneHeight, settings.sceneWidth);
 };
 
 Game.prototype.start = function() {
-    this.mainScene.makeNew(20, 10);
-    this.pausedScene.makeNew(20, 10);
+
+	// every next game starts after gameover
+	// calls from controls.js
+
+    this.mainScene.makeNew(settings.sceneHeight, settings.sceneWidth);
+    this.pausedScene.makeNew(settings.sceneHeight, settings.sceneWidth);
     this.pausedScene.colorRandomly();
     this.shuffle(this.tetroBar);
     this.shuffle(this.nextTetroBar);
     this.step = 0;
     this.curTetro = new Tetromino();
-    
     this.over = false;
     this.play();
     display.nextTetro();
 };
 
 Game.prototype.play = function() {
+
+	// game starts from Game.prototype.start 
+	// stops when game.over = true
+
     game.mainScene.mergeWith(this.curTetro);
     var timer = setTimeout(function run() {
         if (game.over == false) {
@@ -37,6 +52,9 @@ Game.prototype.play = function() {
 };
 
 Game.prototype.shuffle = function(o) {
+
+	// shuffles a tetroBar and nextTetroBar
+
     for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
 };
