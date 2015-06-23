adea791b57757bd97170d7275d60c659e61542a9
Mary <Mary-Ya@users.noreply.github.com>
2015-06-23 11:47:15 +0800

    Update tetromino.js

--- js/tetromino.js
+++ js/tetromino.js
@@ -1,22 +1,73 @@
 var Tetromino = function() {
+
+	// call from Tetromino.prototype.move
+	// and Game.prototype.start
+
     this.newTetromino();
 };
 
+Tetromino.prototype.newTetromino = function() {
+
+	//calls from constructor
+
+    superList = settings.figures;
+
+    /*0 - I - red
+      1 - T - light blue
+      2 - J - deep blue
+      3 - L - green
+      4 - O - yellow
+      5 - Z - brown
+      6 - S - purple
+	*/
+
+    this.fig = [];
+    this.X = 3;
+    this.Y = 0;
+
+    if (game.step < 7) {
+        this.fig = superList[game.tetroBar[game.step]];
+        game.step++;
+        console.log("next");
+        
+        if (game.step == 6) {
+            game.step = 0;
+            game.tetroBar = game.nextTetroBar;
+            game.shuffle(game.nextTetroBar);
+            console.log(game.tetroBar);
+        }
+    };
+
+    display.nextTetro();
+};
+
 Tetromino.prototype.move = function(onScene, ofsX, ofsY) {
+
+	// move tetromino on scene to
+
     if (game.over !== true && game.pause !== true) {
+    // if game in current
         var nextX = this.X + ofsX;
         var nextY = this.Y + ofsY;
         onScene.deleteTetro(this);
+        // delete this tetromino to use check
         var cantMove = onScene.check(nextX, nextY, this.fig);
+        // check if we can put it on the next coordinates
         onScene.mergeWith(this);
+        // put it back to avoid disappearance
         if (cantMove == false) { 
+        	// if it's OK 
             onScene.deleteTetro(this);
             this.X = nextX;
             this.Y = nextY;
             onScene.mergeWith(this);
-        } else { // если препятствия
-            if (ofsY !== 0) { // если движение по Y
+            // put it on the next coordinates
+        } else { 
+        // if cant move
+            if (ofsY !== 0) { 
+            // moves on Y
                 onScene.cutLines();
+                // reduse the lines and create new tetromino
                 game.curTetro = new Tetromino();
                 //display.info("#nextFigure", "Next figure: " + game.curTetro.fig);
                 cantMove = onScene.check(3, 0, this.fig);
@@ -25,6 +76,7 @@ Tetromino.prototype.move = function(onScene, ofsX, ofsY) {
                 onScene.mergeWith(game.curTetro);
                 display.scene(onScene);
                 if (cantMove !== false) {
+                	// if no place to add new tetromino - game over
                     game.over = true;
                     console.log("GameOver");
                     game.pausedScene.colorRandomly();
@@ -38,6 +90,9 @@ Tetromino.prototype.move = function(onScene, ofsX, ofsY) {
 };
 
 Tetromino.prototype.dropOn = function(scene) {
+
+	// to drop the figure down
+
     var curStep = game.step;
     while (curStep === game.step) {
         this.move(scene, 0, 1);
@@ -45,11 +100,15 @@ Tetromino.prototype.dropOn = function(scene) {
 };
 
 Tetromino.prototype.turnOn = function() {
-    if (game.over !== true) {
+
+	// to turn the figure
+
+   if (game.over !== true && game.pause !== true) {
         game.mainScene.deleteTetro(this);
         var newWidth = this.fig.length;
         var newLength = this.fig[0].length;
         var b = new Array(newLength);
+
         for (var i = 0; i < newLength; i++) {
             b[i] = new Array(newWidth);
             for (var j = 0; j < newWidth; j++) {
@@ -60,61 +119,14 @@ Tetromino.prototype.turnOn = function() {
             for (var j = 0; j < newWidth; j++) {
                 b[i][j] = this.fig[newWidth - 1 - j][i];
             };
-        var cantMove = game.mainScene.check(this.X, this.Y, b)
+
+        var cantMove = game.mainScene.check(this.X, this.Y, b);
         if (cantMove == false) {
             this.fig = b;
         };
+
         game.mainScene.mergeWith(this);
         display.scene(game.mainScene);
-    };
-};
-
-Tetromino.prototype.newTetromino = function() {
-    var superList = [
-        [
-            [1, 1, 1, 1]
-        ], //I
-        [
-            [0, 2, 0],
-            [2, 2, 2]
-        ], //T
-        [
-            [3, 0, 0],
-            [3, 3, 3]
-        ], //J
-        [
-            [0, 0, 4],
-            [4, 4, 4]
-        ], //L
-        [
-            [5, 5],
-            [5, 5]
-        ], //O
-        [
-            [6, 6, 0],
-            [0, 6, 6]
-        ], //Z
-        [
-            [0, 7, 7],
-            [7, 7, 0]
-        ] //S
-    ];
-    this.fig = superList[0];
-    this.X = 3;
-    this.Y = 0;
-    if (game.step < 7) {
-        this.fig = superList[game.tetroBar[game.step]];
-        game.step++;
-        console.log("next");
         
-        if (game.step == 6) {
-            game.step = 0;
-            game.tetroBar = game.nextTetroBar;
-            game.shuffle(game.nextTetroBar);
-            
-            console.log(game.tetroBar);
-        };
     };
-    display.nextTetro();
 };
-
